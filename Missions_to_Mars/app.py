1fa93e2b2e621ae89f5e556df8b2612f8a5ddad5
nathanprows77 <66317477+nathanprows77@users.noreply.github.com>
2020-09-11 10:51:39 -0400

    updated app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    space_data = mongo.db.mars.find_one()
+    mars = mongo.db.mars.find_one()
 
     # Return template and data
-    return render_template("index.html", mars=space_data)
+    return render_template("index.html", mars=mars)
 
 
 # Route that will trigger the scrape function
